0e60eb722724c017b4764f6de6aa4803bf132e89
kcanamar <99228411+kcanamar@users.noreply.github.com>
2024-08-27 19:26:42 -0700

    show single todo

--- server.js
+++ server.js
@@ -23,8 +23,19 @@ app.get('/', (req, res) => {
     res.send('Honey im home')
 })
 
+// INDEX 
 app.get('/todos', (req, res) => {
     res.json(todos)
+})
+
+// SHOW
+app.get('/todos/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+
+    // find our todo based on the request param
+    let foundTodo = todos[id]
+    // respond with the found todo
+    res.json(foundTodo)
 })
 
 // LISTENER
