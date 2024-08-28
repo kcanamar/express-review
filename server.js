4c159387f02d2b53d3be43a82e1a04629f70979b
kcanamar <99228411+kcanamar@users.noreply.github.com>
2024-08-27 19:22:17 -0700

    reading all of the todos

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const morgan = require('morgan')
 const app = express();
 const { PORT = 3000 } = process.env
 
+const todos = [
+    {
+        title: "My first Todo",
+        content: "My first message" 
+    }
+]
+
 // MIDDLEWARE
 ///////////////////
 app.use(morgan('dev'))
@@ -16,6 +23,9 @@ app.get('/', (req, res) => {
     res.send('Honey im home')
 })
 
+app.get('/todos', (req, res) => {
+    res.json(todos)
+})
 
 // LISTENER
 ///////////////////
